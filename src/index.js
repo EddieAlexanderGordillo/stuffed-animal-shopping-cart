73b7e6871a81d433880c8b8bde63d6df10d2533d
Felipe Andrés López <alexgordillo.g@gmail.com>
2022-09-06 21:15:23 -0500

    add functionality to shopping cart

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const menuMobile = document.querySelector('.menu-mobile');
+const navContainer = document.querySelector('.nav-container');
 const menu = document.querySelector('.menu');
 const shoppingCartIcon = document.querySelector('.shopping-cart__submenu img');
 const shoppingCartTable = document.querySelector('.shopping-cart__area');
@@ -6,49 +7,68 @@ menuMobile.addEventListener('click', toggleMenuMobile);
 shoppingCartIcon.addEventListener('click', showCart);
 
 function toggleMenuMobile() {
-  menu.classList.toggle('showMenuMobile');
+  if (!menu.classList.contains('showMenuMobile')) {
+    menu.classList.add('showMenuMobile');
+    navContainer.classList.add('backdropMobile');
+    shoppingCartTable.classList.remove('shopping-cart__area--visible');
+  } else {
+    menu.classList.remove('showMenuMobile');
+    navContainer.classList.remove('backdropMobile');
+  }
 }
 function showCart() {
-  shoppingCartTable.classList.toggle('shopping-cart__area--visible');
+  if (!shoppingCartTable.classList.contains('shopping-cart__area--visible')) {
+    shoppingCartTable.classList.add('shopping-cart__area--visible');
+    menu.classList.remove('showMenuMobile');
+    navContainer.classList.remove('backdropMobile');
+  } else {
+    shoppingCartTable.classList.remove('shopping-cart__area--visible');
+  }
 }
 
 /* Create cards*/
 const productsList = [
   {
     image: '../src/assests/images/curso1.jpg',
-    name: 'da',
-    description: 'lana lalalalalsadkadkask kafojaijijdkdjsjdj',
+    name: 'Peluche1',
+    description: 'Lana tejida a dos agujas',
     price: 20000,
+    id: 1,
   },
   {
     image: '../src/assests/images/curso2.jpg',
-    name: 'la',
-    description: 'lna',
+    name: 'Peluche2',
+    description: 'Lana filter',
     price: 60000,
+    id: 2,
   },
   {
     image: '../src/assests/images/curso3.jpg',
-    name: 'ua',
-    description: 'lra',
+    name: 'Peluche3',
+    description: 'Crochet',
     price: 40000,
+    id: 3,
   },
   {
     image: '../src/assests/images/curso4.jpg',
-    name: 'da',
-    description: 'lana',
+    name: 'Peluche4',
+    description: 'Lana',
     price: 20000,
+    id: 4,
   },
   {
     image: '../src/assests/images/curso5.jpg',
-    name: 'la',
-    description: 'lna',
+    name: 'Peluche5',
+    description: 'Lana filter',
     price: 60000,
+    id: 5,
   },
   {
     image: '../src/assests/images/curso3.jpg',
-    name: 'ua',
-    description: 'lra',
+    name: 'Peluche6',
+    description: 'Lana tejida',
     price: 40000,
+    id: 6,
   },
 ];
 const createCard = (product) => {
@@ -62,10 +82,10 @@ const createCard = (product) => {
     <div class="product-card__info">
       <h3 class="info__title">${product.name}</h3>
       <p class="info__description">${product.description}</p>
-      <p class="info__price">$ ${product.price}</p>
+      <p class="info__price"><span>$ ${product.price} </span>pesos</p>
     </div>
     <div class="product-card__button">
-      <a href="#" class="button-add-card">Agregar al carrito<img src="../src/assests/icons/plus-solid.svg" alt="" /></a>
+      <a href="#" class="button-add-card" data-id="${product.id}">Agregar al carrito<img src="../src/assests/icons/plus-solid.svg" alt="" /></a>
       
     </div>
   </div>`;
@@ -78,3 +98,109 @@ function renderCards() {
   }
 }
 renderCards();
+
+// Variables
+const containerTableCart = document.querySelector('#container__table');
+const containerProductList = document.querySelector('#product-list');
+const productList = document.querySelector('#list-cart tbody');
+const emptyCart = document.querySelector('#empty-cart');
+let cartItems = [];
+
+// Listeners
+loadEventListeners();
+
+function loadEventListeners() {
+  // Dispara cuando se presiona "Agregar Carrito"
+  containerProductList.addEventListener('click', addProduct);
+
+  // Cuando se elimina un producto del carrito
+  containerTableCart.addEventListener('click', deleteProduct);
+
+  // Al Vaciar el carrito
+  emptyCart.addEventListener('click', emptyProducts);
+}
+
+// Funciones
+// Función que añade el producto al carrito
+function addProduct(e) {
+  e.preventDefault();
+  // Delegation para agregar-carrito
+  if (e.target.classList.contains('button-add-card')) {
+    const product = e.target.parentElement.parentElement;
+    // Enviamos el producto seleccionado para tomar sus datos
+    readProductData(product);
+  }
+}
+
+// Lee los datos del producto
+function readProductData(item) {
+  const productInfo = {
+    imagen: item.querySelector('img').src,
+    titulo: item.querySelector('h3').textContent,
+    precio: item.querySelector('.info__price span').textContent,
+    id: item.querySelector('a').getAttribute('data-id'),
+    cantidad: 1,
+  };
+
+  if (cartItems.some((itemProduct) => itemProduct.id === productInfo.id)) {
+    const products = cartItems.map((product) => {
+      if (product.id === productInfo.id) {
+        product.cantidad++;
+        return product;
+      } else {
+        return product;
+      }
+    });
+    cartItems = [...products];
+  } else {
+    cartItems = [...cartItems, productInfo];
+  }
+
+  carritoHTML();
+}
+
+// Elimina el curso del carrito en el DOM
+function deleteProduct(e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('delete-product')) {
+    const productId = e.target.parentElement.getAttribute('data-id');
+
+    // Eliminar del arreglo del carrito
+    cartItems = cartItems.filter((product) => product.id !== productId);
+
+    carritoHTML();
+  }
+}
+
+// Muestra el producto seleccionado en el Carrito
+function carritoHTML() {
+  emptyProducts();
+
+  cartItems.forEach((item) => {
+    const rowcart = document.createElement('tr');
+    rowcart.innerHTML = `
+               <td>  
+                    <img src="${item.imagen}" width=70>
+               </td>
+               <td>${item.titulo}</td>
+               <td>${item.precio}</td>
+               <td>${item.cantidad} </td>
+               <td>
+                    <a href="#" class="delete-product" data-id="${item.id}"><img
+                src="../src/assests/icons/circle-xmark-regular.svg"
+                alt=""
+                // class="delete-product"
+            /></a>
+               </td>
+          `;
+    productList.appendChild(rowcart);
+  });
+}
+
+// Elimina los productos del carrito en el DOM
+function emptyProducts() {
+  while (productList.firstChild) {
+    productList.removeChild(productList.firstChild);
+  }
+}
